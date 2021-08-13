665483671acc5b1c1a372940eae297132e859c9d
wulijian <974393582@qq.com>
2021-08-13 09:31:24 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author boqinglang
+ * @date 2021/8/13 9:24 上午
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
